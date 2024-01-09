8e66672208cbf77b4e4c204400810132472e3683
Dylan Brown <dylanbrown588@gmail.com>
2024-01-08 19:18:08 -0600

    set global variables and hook into the buttons

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+let fungusHP = 100;
+let playerAP = 100;
 
 function onReady() {
     console.log("Ready to go!")
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
-    
-    
+
+    const arcaneScepterButton = document.querySelector('.arcane-scepter');
+    const entangleButton = document.querySelector('.entangle');
+    const dragonBladeButton = document.querySelector('.dragon-blade');
+    const starFire = document.querySelector('.star-fire');
+
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
